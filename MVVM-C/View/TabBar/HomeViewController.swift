c61dbf16c980afc867ed548ff1b1bf8ff8a7abcb
RChandan <radhachandan9027@gmail.com>
2020-08-10 11:42:02 +0530

    Commit w.r.t changes.

--- MVVM-C/View/TabBar/HomeViewController.swift
+++ MVVM-C/View/TabBar/HomeViewController.swift
@@ -10,4 +10,25 @@ extension HomeViewController {
             controller.title = "Details"
         }
     }
+    
+    @IBAction func alertButtonPressed(_ sender: UIButton) {
+        Alert.presentAlert(withTitle: "Hey Alert..!", message: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", actionParameters: [AlertParameters(title: "Action", action: { (action) in
+            print("Perform your action")
+        }, preferredAction: true, actionStyle: .destructive)], controller: self, style: .alert)
+    }
+    
+    @IBAction func popUpButtonPressed(_ sender: UIButton) {
+        CustomPopup().present(message: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", animate: .affineIn)
+    }
+    
+    @IBAction func slideViewButtonPressed(_ sender: UIButton) {
+        _ = AnimatedView(message: .internet, postion: .top, bgColor: UIColor().appColor())
+    }
+    
+    @IBAction func loadingButtonPressed(_ sender: UIButton) {
+        LoadingIndicator.shared.loading(step: .start(animate: true))
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            LoadingIndicator.shared.loading(step: .success(animate: true))
+        }
+    }
 }
