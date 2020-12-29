cc2ccb4938d0f74f4785bc7efcc34a2d9ab929ae
ChandanChilamkurthy <radhachandan.chilamkurthy@prowesssoft.com>
2020-12-29 23:15:01 +0530

    Commit w.r.t Access Modifiers

--- MVVM-C/SocialProfilesSignIn/FirebaseAnalytics.swift
+++ MVVM-C/SocialProfilesSignIn/FirebaseAnalytics.swift
@@ -2,22 +2,22 @@ import Foundation
 import FirebaseAnalytics
 
 protocol FBAnalyticsProtocol {
-    func logScreenName()
-    func logFunctionName(method: String)
-    func logEvent(method: String)
+    static func logScreenName()
+    static func logFunctionName(method: String)
+    static func logEvent(method: String)
 }
 
 struct FBAnalytics: FBAnalyticsProtocol {
     
-    func logScreenName() {
+    static func logScreenName() {
         Analytics.logEvent(AnalyticsEventScreenView, parameters: [AnalyticsParameterScreenName: "screenName", AnalyticsParameterScreenClass: "screenClass"])
     }
     
-    func logFunctionName(method: String) {
+    static func logFunctionName(method: String) {
         Analytics.logEvent(AnalyticsParameterMethod, parameters: [AnalyticsParameterMethod: method, AnalyticsParameterScreenClass: "screenClass"])
     }
     
-    func logEvent(method: String) {
+    static func logEvent(method: String) {
         Analytics.logEvent(AnalyticsEventSelectContent, parameters: [AnalyticsParameterScreenName: "screenName", AnalyticsParameterMethod: method, AnalyticsParameterItemID: "ItemID", AnalyticsParameterItemName: "Title", AnalyticsParameterContentType: "Something"])
     }
 }
