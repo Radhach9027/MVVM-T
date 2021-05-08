39276d613f890d724836dfa98eac565d9fbba5d5
ChandanChilamkurthy <radhachandan.chilamkurthy@prowesssoft.com>
2021-05-08 18:41:46 +0530

    Commit w.r.t folder deletions

--- MVVM-T/DependencyContainer/DependencyBuilder.swift
+++
@@ -1,4 +0,0 @@
-@_functionBuilder public struct DependencyBuilder {
-    static func buildBlock(_ dependency: Dependency) -> Dependency { dependency }
-    static func buildBlock(_ dependencies: Dependency...) -> [Dependency] { dependencies }
-}
